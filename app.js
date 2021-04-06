8a55f8350bd2a45112b5afe3a684c4e5f8e4563c
lil-armstrong <oebong1@gmail.com>
2021-04-06 11:01:00 +0100

    UI fixes

--- app.js
+++ app.js
@@ -6,37 +6,40 @@ const io = require('socket.io')(http);
 const port = 4000;
 
 function uuid() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-    var r = (Math.random() * 16) | 0,
-    v = c == "x" ? r: (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+        var r = (Math.random() * 16) | 0,
+            v = c == "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
 }
 
 app.use(express.static(path.join(__dirname, 'src')));
 // console.log(path.join(__dirname, 'public','index.html'))
-app.get('/', (req, res)=> {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
 io.on('connection', (socket) => {
-  let id = socket.client.conn.id;
-  console.log(`(User: ${id}) connected`);
-  socket.on('chat message', (msg) => {
-    socket.broadcast.emit('chat message', {
-      body: msg,
-      id : uuid(),
-      type:'recipient'
-    })
-  });
-  socket.on('typing', ()=>{
-    socket.broadcast.emit('user-typing');
-  })
-  socket.on('disconnect', () => {
-    console.log(`(User: ${id}) disconnected`);
-  });
+    let id = socket.client.conn.id;
+    console.log(`(User: ${id}) connected`);
+    socket.on('chat-message', (msg) => {
+        socket.broadcast.emit('chat-message', {
+            body: msg,
+            id: uuid(),
+            type: 'recipient'
+        })
+    });
+    socket.on('is-typing', () => {
+        socket.broadcast.emit('is-typing');
+    });
+    socket.on('not-typing', () => {
+        socket.broadcast.emit('not-typing');
+    });
+    socket.on('disconnect', () => {
+        console.log(`(User: ${id}) disconnected`);
+    });
 });
 
-http.listen(port, ()=> {
-  console.log('Server running on port '+port)
+http.listen(port, () => {
+    console.log('Server running on port ' + port)
 })
